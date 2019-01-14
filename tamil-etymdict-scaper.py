02ff47c7af28d3f89bda5a2020e00d581de341f7
M Selvakumar <selva.developer@gmail.com>
2019-01-14 16:26:46 +0530

    minor cleanup

--- tamil-etymdict-scaper.py
+++ tamil-etymdict-scaper.py
@@ -30,8 +30,8 @@ def mkdir(path):
 
 
 URL = 'http://stream1.tamilvu.in/etytamildict/TamilDemo.aspx'
-
-
+WORDS_FILEPATH = 'etymdict_lexicon.csv'
+LEXICON_FILEPATH = 'length_words_first.cleaned.txt'
 # tags, classes, ids for content extraction
 INPUT_TEXTBOX_ID = 'txtNames1'
 
@@ -46,14 +46,17 @@ def gather_words(wd):
     WORDS = Counter()
     input_text = INITIAL_KEYWORD
     
-    lexicon = []
-    with open('length_words_first.cleaned.txt', 'r') as f:
-        lines = [l.strip().split('|')[0] for l in f.readlines() if len(l) < 5 and len(l) > 2]
-        lexicon.extend(lines)
+    __LEXICON__ = []
+    with open(LEXICON_FILEPATH, 'r') as f:
+        for l in f.readlines():
+            l = l.strip()
+            if len(l) < 5 and len(l) > 2:
+                __LEXICON__.append(l.split('|')[0])
+            
 
     words_already_lookedup = {}
 
-    for i in tqdm(lexicon):
+    for i in tqdm(__LEXICON__):
         input_text = i
         for max_len in reversed(range(1, 6)):
             try:
@@ -63,65 +66,98 @@ def gather_words(wd):
                         continue
 
                     words_already_lookedup[input_text] = 1
-                    input_textbox = wd.find_element_by_id( INPUT_TEXTBOX_ID)
-                    input_textbox.clear()
+
                     print('sending ', input_text)
+                    input_textbox = wd.find_element_by_id(INPUT_TEXTBOX_ID)
+                    input_textbox.clear()
                     input_textbox.send_keys(input_text + ' ')
                     WebDriverWait(wd, 10)
                     input_textbox.send_keys(Keys.BACK_SPACE)
-                    input_text = input_text[:-1]
 
-                    # Wait for the dynamically loaded elements to show up
                     WebDriverWait(wd, 10).until(
-                        EC.visibility_of_element_located((By.CLASS_NAME, WORDS_POPUP_LIST_CLASS)))
+                        EC.visibility_of_element_located(
+                            (By.CLASS_NAME, WORDS_POPUP_LIST_CLASS)))
 
                     html_page = wd.page_source
-                    soup = BeautifulSoup(html_page, 'lxml')
-                    new_words = [i.text for i in soup.find_all(class_=WORDS_POPUP_LIST_ITEMS_CLASS)]
+                    soup      = BeautifulSoup(html_page, 'lxml')
+                    new_words = [i.text for i in
+                                 soup.find_all(class_=WORDS_POPUP_LIST_ITEMS_CLASS)]
+                    
                     WORDS.update(new_words)
+                    
+                    input_text = input_text[:-1]
 
+                    
                 print('extracted {} words'.format(len(WORDS)))          
 
-                with open('etymdict_lexicon.csv', 'w') as f:
+                with open(WORDS_FILEPATH, 'w') as f:
                     f.write('\n'.join(WORDS.keys()))
                     f.flush()
 
             except:
                 log.exception('ERROR: {}'.format(input_text))
 
-                with open('etymdict_lexicon.csv', 'w') as f:
+                with open(WORDS_FILEPATH, 'w') as f:
                     f.write('\n'.join(WORDS.keys()))
                     f.flush()
 
     wd.quit()
 
+class wait_for_the_attribute_value(object):
+    """
+    https://stackoverflow.com/questions/43813170/using-selenium-webdriver-to-wait-the-attribute-of-element-to-change-value
+    """
 
+    def __init__(self, locator, attribute, value):
+        self.locator = locator
+        self.attribute = attribute
+        self.value = value
 
-def gather_word_meanings(wd, words):
-    words_already_lookedup = {}
+    def __call__(self, driver):
+        try:
+            elements  = EC._find_elements(driver, self.locator)
+            for element in elements:
+                attribute = element.get_attribute(self.attribute)
+                if attribute == self.value:
+                    return True
+                
+            return False
+        except StaleElementReferenceException:
+            log.exception()
+            return False
 
+        
+def gather_word_meanings(wd, words=[]):
+    if len(words) < 1:
+        words = open(WORDS_FILEPATH).read().split('\n')[:10]
+        log.info('loaded {} words from {}'.format(len(words), WORDS_FILEPATH))
+        
     for i in tqdm(words):
         input_text = i
         try:
-            words_already_lookedup[input_text] = 1
-            input_textbox = wd.find_element_by_id( INPUT_TEXTBOX_ID)
-            input_textbox.clear()
+            
             print('sending ', input_text)
+            input_textbox = wd.find_element_by_id(INPUT_TEXTBOX_ID)
+            input_textbox.clear()
+            input_textbox = wd.find_element_by_id(INPUT_TEXTBOX_ID)
             input_textbox.send_keys(input_text)
             
             submit_btn = wd.find_element_by_id(SUBMIT_BTN_ID)
             submit_btn.click()
             wd.implicitly_wait(10)
             
-            # Wait for the dynamically loaded elements to show up
             WebDriverWait(wd, 10).until(
-                EC.visibility_of_element_located((By.CLASS_NAME, WORDS_POPUP_LIST_CLASS)))
+                wait_for_the_attribute_value((By.TAG_NAME, 'span'),
+                                             'id',
+                                             'lblResults'))
             
             html_page = wd.page_source
             soup = BeautifulSoup(html_page, 'lxml')
-            new_words = [i.text for i in soup.find_all(class_=WORDS_POPUP_LIST_ITEMS_CLASS)]
-            WORDS.update(new_words)
-            
+            span =soup.find('span', id='lblResults')
+            #print(span.prettify())
+            for i in span:
+                print(i)
+                print('===')
             
         except:
             log.exception('ERROR: {}'.format(input_text))
